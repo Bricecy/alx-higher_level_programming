339cf8226b79bd3de84752f4e48dd12b1d02cd8d
Bricecy <brigidwambua97@gmail.com>
2023-03-14 11:54:09 -0700

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list {
-        println!("{}", num);
-    }
-}
-
-
-def main() {
-    let my_list = vec![1, 2, 3, 4, 5];
-    print_list_integer(&my_list);
-}
-
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
