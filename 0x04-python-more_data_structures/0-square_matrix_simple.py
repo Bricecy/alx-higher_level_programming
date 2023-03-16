886cfbf86e5bfa55c265cdd4e04fcb549a4acb06
Bricecy <brigidwambua97@gmail.com>
2023-03-16 00:53:28 -0700

    Updates

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    return([list(map(lambda x: x**2,row)) for row in matrix])
+    def square_cells(array):
+        result = []
+        for cell in array:
+            result.append(cell ** 2)
+        return result
+    return list(map(square_cells, matrix))
