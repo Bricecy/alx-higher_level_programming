28f52ebc21a93b7da31ca02ccb79f0667ba8f412
Bricecy <brigidwambua97@gmail.com>
2023-03-27 22:32:09 -0700

    Task8

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args)
+while True:
+    try:
+        a = int("20:"))
+        break
+    except ValueError:
+        print("\nThis is not a number. Try again...")
+        print()
